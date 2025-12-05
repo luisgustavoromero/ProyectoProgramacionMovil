6acff27b5d4b0a325cd31a14bc4b20d216b36d82
GUSTAVO\luisg <romero.luisg@unitec.edu>
2025-12-05 17:34:05 -0600

    Implementacion de .env y creacion de authcontext para separa datos publicos del perfil y datos de login. (authcontext pendiente de integrarlo en el login)

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+//Pendiente de implentar en caso de usar AuthContext
+import React, {createContext, useContext, useEffect, useState} from 'react'
+import { supabase } from '../services/supabaseClient'
+import { Alert } from 'react-native';
+
+type User = {
+    id: string,
+    email: string,
+    token: string
+} | null
+
+type AuthContextType = {
+    user : User | null,
+    isAllowed: boolean,
+    login: (email:string, password: string) => Promise<void>,
+    logout: () => void,
+}
+
+const AuthContext = createContext<AuthContextType|null>(null)
+
+export const useAuth = () => {
+    const context =useContext(AuthContext);
+    if(!context) throw new Error('useAuth debe usarse dentro de Authprovider')
+    return context
+}
+
+export const AuthProvider = ({children}: {children: React.ReactNode}) => {
+    const [user, setUser] = useState<User>(null)
+    const [isAllowed, setIsAllowed] = useState<boolean>(false)
+
+
+    const login = async (email: string, password: string) =>{
+
+    }
+
+    const logout = async () =>{
+        
+    }
+
+    return(
+        <AuthContext.Provider value={{user, isAllowed, login, logout}}></AuthContext.Provider>
+    )
+}
--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,8 +1,7 @@
 //tarea: implementar .env
 
 import { createClient } from "@supabase/supabase-js";
+import {SUPABASE_URL, SUPABASE_ANON_KEY} from "@env"
 
-const SUPABASE_URL = 'https://zmzmesnvqdmxyvuvwbvi.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inptem1lc252cWRteHl2dXZ3YnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjIzODgsImV4cCI6MjA3OTU5ODM4OH0.UoGfUB_DdoW2VTScAaxKB5tMvC2Yd0pXPXraqHGt074'
 
 export const supabase = createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
